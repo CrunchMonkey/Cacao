ea54cf64c47b7bcd24b05aa2a8796ac1f6c13438
이승렬 <dltmdfuf95@naver.com>
2023-12-16 11:39:15 +0900

    jpa연동

--- src/main/java/com/example/demo/controller/mainController.java
+++ src/main/java/com/example/demo/controller/mainController.java
@@ -1,13 +1,35 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.LOST_NOTI;
+import com.example.demo.entity.LOST_NOTIRepository;
+import org.junit.After;
+import org.junit.Test;
+
 @RestController
 public class mainController {
+	LOST_NOTIRepository lostNotiRepository;
+	
+	@Autowired
+	public mainController(LOST_NOTIRepository lostNotiRepository) {
+		this.lostNotiRepository = lostNotiRepository;
+	}
+	
+	@After //Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드
+	public void cleanUp() {
+		lostNotiRepository.deleteAll();
+	}
+	
+	@Test
+	public void test() {
+		lostNotiRepository.save(LOST_NOTI.builder().noti_kind_cd("dd").build());
+	}
 	
 	@RequestMapping("/hello")
-	public String test() {
+	public String test1() {
 		return "hello";
 	}
 	
---
+++ src/main/java/com/example/demo/entity/LOST_NOTIRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LOST_NOTIRepository  extends JpaRepository<LOST_NOTI, Long>{//연결한 엔티티<이름, pk 변수형>
+
+}
